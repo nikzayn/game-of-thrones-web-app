95e93b29ee2658a23f61586f0c282acad55af544
nikzayn <nikhilvaidyar1997@gmail.com>
2020-04-30 01:52:35 +0530

    Endpoints --added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongodb = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -23,17 +22,61 @@ const dbName = process.env.MONGO_DB;
 const MONGO_URL = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
 
 
+//Battle location
 app.get('/list', (req, res) => {
     const collection = db.collection('battles');
-    collection.find({}).toArray(function (err, docs) {
-        res.send(docs);
+    collection.distinct('location', (err, result) => {
+        if (!err) {
+            res.send(result);
+        }
+        return err;
     });
-
 })
 
-// app.get('/count', (req, res) => {
-//     collection.
-// })
+//Total number of battles occured
+app.get('/count', (req, res) => {
+    const collection = db.collection('battles');
+    var total = 0;
+    collection.find({}).toArray((err, result) => {
+        if (!err) {
+            for (var i = 0; i < result.length; i++) {
+                total += result[i].battle_number;
+            }
+
+            res.send({ total });
+        }
+        return err;
+    })
+})
+
+// Return list of battles where 'attacker_king' or 'defender_king' was 'Robb Stark'
+app.get('/search', (req, res) => {
+    const collection = db.collection('battles');
+    const { king, location, type } = req.query;
+    const params = {
+        attacker_king: king,
+        defender_king: king,
+        location,
+        battle_type: type
+    }
+    collection.find({
+        "$and": [{
+            $or: [
+                { "attacker_king": `${params.attacker_king}` },
+                { "defender_king": `${params.defender_king}` }
+            ]
+        }, {
+            "location": `${params.location}`,
+            "battle_type": `${params.battle_type}`
+        }]
+    }).toArray((err, result) => {
+        if (!err) {
+            res.send(result);
+            console.log(req.query);
+        }
+        return err;
+    });
+})
 
 
 app.listen(port, () => {
